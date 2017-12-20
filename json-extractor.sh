ca2ee70b8d5f4f56a10d27d24830d9219d591b0c
José Ángel Carvajal Soto <carvajal@fit.fraunhofer.de>
2017-12-20 14:41:27 +0000

    the extractor also publish

--- json-extractor.sh
+++ json-extractor.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-# cloning and building apache code
-#URL="http://dpa:8319/v2/api-docs?group=LinkSmart%20(R)%20IoT%20Learning%20Agent"
-#FILE=api-docs.json
+
+git clone https://${SERVER_USERNAME}:${SERVER_PASSWORD}@code.linksmart.eu/scm/${REPO} -b ${branch} code
+
+cd code
+
 if [ -z ${URL} ]; then 
 	URL="http://dpa:8319/v2/api-docs?group=LinkSmart%20(R)%20IoT%20Learning%20Agent"
 fi
@@ -14,3 +16,7 @@ until [ -s ${FILE} ]; do
 	cat err
 	sleep 1
 done;
+
+git add ${FILE}
+git commit -m "AUTOMATIC COMMIT: updating Open API"
+git push 
