b6914b9f3f14c183b6431d78724d6a3d1812d035
Jose Angel <carvajal@fit.fhg.de>
2017-12-27 16:56:50 +0100

    verbose the RC script

--- RC-client-release.sh
+++ RC-client-release.sh
@@ -11,6 +11,7 @@ echo "creating clients"
 mvn install
 
 echo "renaming the clients"
+
 sed -i '0,/<groupId>io.swagger<\/groupId>/ s/<groupId>io.swagger<\/groupId>/<groupId>eu.linksmart.tools.registration<\/groupId>/' 'client/pom.xml';
 sed -i '0,/<artifactId>swagger-java-client<\/artifactId>/ s/<artifactId>swagger-java-client<\/artifactId>/<artifactId>service-catalog-client<\/artifactId>/' 'client/pom.xml';
 sed -i '0,/<name>swagger-java-client<\/name>/ s/<name>swagger-java-client<\/name>/<name>service-catalog-client<\/name>/' 'client/pom.xml';
@@ -19,5 +20,5 @@ sed -i s/'<version>1.0.0<\/version>'/'<version>'`jq -r .info.version ../../apido
 echo "moving to /data/client/java/client"
 cd client
 
-echo "deploying
+echo "deploying"
 mvn deploy:deploy-file -D"pomFile=pom.xml" -D"file=target\service-catalog-client-2.2.jar"  -D"repositoryId=releases" -"Durl=https://nexus.linksmart.eu/repository/maven-releases/";
