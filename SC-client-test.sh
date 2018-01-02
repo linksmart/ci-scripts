286669e35cb3a4989f0334121399127701dd88d8
Jose Angel <carvajal@fit.fhg.de>
2018-01-02 14:51:57 +0100

    add run test to sh

--- SC-client-test.sh
+++ SC-client-test.sh
@@ -2,4 +2,4 @@
 
 echo "testing"
 cd /data/integration-test/registration
-mvn -D'ls.sc.version'=`jq -r .info.version /data/apidoc/swagger.json` test
+mvn -Dtest=* -D'ls.sc.version'=`jq -r .info.version /data/apidoc/swagger.json` test
