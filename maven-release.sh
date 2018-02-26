b1478f6132c66ba576b1bc3fb8e43fc5ec74caf5
José Ángel Carvajal Soto <carvajal@fit.fraunhofer.de>
2018-02-26 17:44:23 +0000

    update dependencies version of the agen

--- maven-release.sh
+++ maven-release.sh
@@ -7,5 +7,5 @@ git clone https://${SERVER_USERNAME}@code.linksmart.eu/scm/${REPO} -b ${branch} 
 cd /data/code
 
 
-mvn -s /maven/settings.xml -B release:prepare -DreleaseVersion=${version} -Dpassword=${SERVER_PASSWORD} -Dusername=${SERVER_USERNAME} 
-mvn -s /maven/settings.xml -B release:perform -Dgoals=deploy  -DreleaseVersion=${version} -Dpassword=${SERVER_PASSWORD} -Dusername=${SERVER_USERNAME} 
+mvn -s /maven/settings.xml -B release:prepare -DreleaseVersion=${version} -Dlinksmart.commons.version=${linksmart_commons_version} -Dls.se.agents.version==${version} -Dpassword=${SERVER_PASSWORD} -Dusername=${SERVER_USERNAME} 
+mvn -s /maven/settings.xml -B release:perform -Dgoals=deploy  -DreleaseVersion=${version} -Dlinksmart.commons.version=${linksmart_commons_version} -Dls.se.agents.version==${version} -Dpassword=${SERVER_PASSWORD} -Dusername=${SERVER_USERNAME} 
