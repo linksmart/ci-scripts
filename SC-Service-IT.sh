9e162ae2885fdaaa916c5181ba23ee4a3ec34dd1
Jose Angel <carvajal@fit.fhg.de>
2018-06-27 14:42:19 +0200

    modify la-sc it such do not req voulumes

--- SC-Service-IT.sh
+++ SC-Service-IT.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+git clone git clone https://code.linksmart.eu/scm/la/data-processing-agent.git --branch dev /data/agent
+git clone https://code.linksmart.eu/scm/sc/service-catalog.git /data/sc
+
 echo "testing"
 cd /data/sc/integration-test/registration3rdService
 
