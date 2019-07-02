0896fb8a3c85f820c81cc29a7b9d0e4844e9afeb
Farshid Tavakolizadeh <email@farshid.ws>
2019-07-02 17:49:13 +0200

    ldflags support

--- go/go-build.sh
+++ go/go-build.sh
@@ -1,27 +1,39 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# EXAMPLES
+# Build for default platforms:
+# NAME=app ./go-build.sh
+# Build for specific platforms:
+# NAME=app PLATFORMS="linux/arm64 linux/arm" ./go-build.sh
+# Build with ldflags:
+# NAME=app LDFLAGS="-X main.Version=$version -X main.BuildNumber=$buildnum" ./go-build.sh
+
+echo "Go Builder: https://github.com/linksmart/ci-scripts/blob/master/go/go-build.sh"
+
+output_dir=bin
 
 if [[ -z "$NAME" ]]; then
-  echo "usage: PLATFORMS=\"windows/amd64 darwin/amd64 linux/amd64 linux/arm\" NAME=app $0"
+  echo "usage: NAME=app sh go-build.sh"
   exit 1
 fi
 
 if [[ -z "$PLATFORMS" ]]; then
-  PLATFORMS="windows/amd64 darwin/amd64 linux/amd64 linux/amd64 linux/arm"
+  PLATFORMS="windows/amd64 darwin/amd64 linux/amd64 linux/amd64 linux/arm64 linux/arm"
   echo "Using default platforms: $PLATFORMS"
 fi
 
 for platform in $PLATFORMS
 do
-    echo "Building for $platform"
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name=$NAME'-'$GOOS'-'$GOARCH
+    output_name=$output_dir'/'$NAME'-'$GOOS'-'$GOARCH
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
+    echo "Building $output_name"
 
-    GOOS=$GOOS GOARCH=$GOARCH go build -o bin/$output_name
+    go build -mod=vendor -ldflags "$LDFLAGS" -o $output_name
     if [ $? -ne 0 ]; then
         echo "An error has occurred! Aborting the script execution..."
         exit 1
