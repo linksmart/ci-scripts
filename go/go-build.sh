bd0d4df0ac908e3095eabc128e2da612b8e616fb
Farshid Tavakolizadeh <email@farshid.ws>
2019-07-03 12:06:44 +0200

    bugfix: export variables to go build

--- go/go-build.sh
+++ go/go-build.sh
@@ -11,7 +11,9 @@
 echo "Go Builder: https://github.com/linksmart/ci-scripts/blob/master/go/go-build.sh"
 
 output_dir=bin
-GO111MODULE=on
+
+export GO111MODULE=on
+export CGO_ENABLED=0
 
 if [[ -z "$NAME" ]]; then
   echo "usage: NAME=app sh go-build.sh"
@@ -19,22 +21,22 @@ if [[ -z "$NAME" ]]; then
 fi
 
 if [[ -z "$PLATFORMS" ]]; then
-  PLATFORMS="windows/amd64 darwin/amd64 linux/amd64 linux/amd64 linux/arm64 linux/arm"
+  PLATFORMS="windows/amd64 darwin/amd64 linux/amd64 linux/arm64 linux/arm"
   echo "Using default platforms: $PLATFORMS"
 fi
 
 for platform in $PLATFORMS
 do
     platform_split=(${platform//\// })
-    GOOS=${platform_split[0]}
-    GOARCH=${platform_split[1]}
+    export GOOS=${platform_split[0]}
+    export GOARCH=${platform_split[1]}
     output_name=$output_dir'/'$NAME'-'$GOOS'-'$GOARCH
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
     echo "Building $output_name"
 
-    go build -mod=vendor -tags="netgo -a -v" -ldflags "-X main.Version=$VERSION -X main.BuildNumber=$BUILDNUM" -o $output_name
+    go build -mod=vendor -ldflags "-X main.Version=$VERSION -X main.BuildNumber=$BUILDNUM" -o $output_name
     if [ $? -ne 0 ]; then
         echo "An error has occurred! Aborting the script execution..."
         exit 1
