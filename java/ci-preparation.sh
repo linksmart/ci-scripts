534c1e40497df57d9dfa65cdd4da52ef57ea2aba
Jose Angel <carvajal@fit.fhg.de>
2019-08-23 15:57:03 +0200

    add ci preparation script

---
+++ java/ci-preparation.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -i
+
+git clone https://github.com/linksmart/ci-scripts.git ci
+cp -ar ci/java/.mvn .
+cp ci/java/.travis.settings.xml .
+cp ci/java/.versionScript.py .
+
+if [ "$TRAVIS_BRANCH" = "release" ]
+ then
+   export ARTIFACT_VERSION=`curl -s https://raw.githubusercontent.com/linksmart/ci-scripts/master/java/.versionScript.py | python3`
+fi
