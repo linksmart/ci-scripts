09930d53f0ae4a8291a91b12f5f2e65547771412
Jose Angel <carvajal@fit.fhg.de>
2019-08-23 15:35:26 +0200

    creating release scripts

--- java/maven-release.sh
+++ java/maven-release.sh
@@ -1,11 +1,30 @@
-#!/bin/sh
-# cloning and building apache code
-cd /data/ 
+#!/bin/sh -i
 
-git clone https://${SERVER_USERNAME}@code.linksmart.eu/scm/${REPO} -b ${branch} /data/code
+# deploying artifacts in nexus
+mvn deploy
 
-cd /data/code
+# configuring git
+git config --global user.email 'travis@travis-ci.org'
+git config --global user.name 'Travis CI'
 
+# moving realocate head
+git branch tmp
+git checkout release
+git merge tmp
+git branch -d tmp
 
-mvn -s /maven/settings.xml -B release:prepare -DreleaseVersion=${version} -Dlinksmart.commons.version=${linksmart_commons_version} -Dls.se.agents.version==${version} -Dpassword=${SERVER_PASSWORD} -Dusername=${SERVER_USERNAME} 
-mvn -s /maven/settings.xml -B release:perform -Dgoals=deploy  -DreleaseVersion=${version} -Dlinksmart.commons.version=${linksmart_commons_version} -Dls.se.agents.version==${version} -Dpassword=${SERVER_PASSWORD} -Dusername=${SERVER_USERNAME} 
+# tagging release
+git tag v$ARTIFACT_VERSION
+git commit -am \"[skip travis] AUTOMATIC COMMIT: tagging version\"
+
+# prepearign master for new snapshot version
+git fetch origin master:master
+git branch -a
+git checkout master
+git merge release
+python3 .versionScript.py
+git commit -am \"[skip travis] AUTOMATIC COMMIT: preparing new SNAPSHOT\"
+
+# commit all changes and tags
+git push https://${GH_TOKEN}@github.com/linksmart/linksmart-java-utils.git --all
+git push https://${GH_TOKEN}@github.com/linksmart/linksmart-java-utils.git --tags
