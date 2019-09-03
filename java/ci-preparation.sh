d7ab98e0f9bed2aa1292396aa1ad4154e829b8b8
Jose Angel <carvajal@fit.fhg.de>
2019-09-03 15:32:30 +0200

    increase verbosity

--- java/ci-preparation.sh
+++ java/ci-preparation.sh
@@ -1,19 +1,21 @@
 #!/bin/sh -e
 
 git clone https://github.com/linksmart/ci-scripts.git ci
+echo "copying maven files ..."
 cp -ar ci/java/.mvn .
 cp ci/java/.travis.settings.xml .
-cp ci/java/.versionScript.py .
-cp ci/java/maven-release.sh .
-cp ci/git-realocate-head.sh .
-chmod +x maven-release.sh
-chmod +x git-realocate-head.sh
+echo "copying scripts ..."
+cp ci/* .
+cp ci/java/* .
+echo "granting execution rights ..."
+chmod +x *.sh
 
+echo "realocating head ..."
 . git-realocate-head.sh
 
 if [ "$TRAVIS_BRANCH" = "release" ]
  then
+   echo "releasing..."
    export ARTIFACT_VERSION=`python3 .versionScript.py`
    [ ! "$ARTIFACT_VERSION" ] && exit 1
-   echo "releasing..."
 fi
