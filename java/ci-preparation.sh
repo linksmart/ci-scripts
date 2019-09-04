efca99654d63cb9422b94f1509faca90a05be39b
Jose Angel <carvajal@fit.fhg.de>
2019-09-04 11:30:07 +0200

    improve script customization and printing

--- java/ci-preparation.sh
+++ java/ci-preparation.sh
@@ -1,20 +1,29 @@
 #!/bin/sh -e
 
 git clone https://github.com/linksmart/ci-scripts.git ci
-echo "copying maven configuration files  ..."
+printf "copying maven configuration files  ..."
 cp -ar ci/java/.mvn .
 cp ci/java/.travis.settings.xml .
+echo "done."
 
-echo "copying ci scripts  ..."
+printf "copying ci scripts  ..."
 cp ci/git-realocate-head.sh .
 cp ci/java/versionScript.py .
 cp ci/java/maven-release.sh .
+echo "done."
 
-echo "granting execution rights ..."
+printf "granting execution rights ..."
 chmod +x *.sh
+echo "done."
 
-echo "realocating head ..."
-. git-realocate-head.sh
+if [ "$1" != "" ]; then
+  echo "Skipping head realoction"
+else
+  printf "realocating head ..."
+  . git-realocate-head.sh
+  echo "done."
+fi
+
 
 if [ "$TRAVIS_BRANCH" = "release" ]
  then
