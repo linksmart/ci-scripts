5df84150179fa32be0ee2ceedeb18c46ec909646
BlubbFish <git@blubbfish.net>
2020-05-19 15:12:48 +0200

    Logrotate-Build-Script finished

---
+++ deb/logrotate-build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# EXAMPLES
+usage() {
+  echo "usage: " \
+  "NAME=app " \
+  "DEBBUILDPATH=\$HOME/deb " \
+  "sh pre-post-build.sh"
+  exit 1
+}
+
+echo "DEB Builder logrotate-file: https://github.com/linksmart/ci-scripts/blob/master/deb/logrotate-build.sh"
+
+if [[ -z "$NAME" ]]; then
+  usage  
+fi
+
+if [[ -z "$DEBBUILDPATH" ]]; then
+  usage
+fi
+
+mkdir -p $DEBBUILDPATH/etc/logrotate.d
+
+echo "/var/log/loramap.log {"  > $DEBBUILDPATH/etc/logrotate.d/$NAME.conf
+echo "	compress"             >> $DEBBUILDPATH/etc/logrotate.d/$NAME.conf
+echo "	copytruncate"         >> $DEBBUILDPATH/etc/logrotate.d/$NAME.conf
+echo "	daily"                >> $DEBBUILDPATH/etc/logrotate.d/$NAME.conf
+echo "	delaycompress"        >> $DEBBUILDPATH/etc/logrotate.d/$NAME.conf
+echo "	missingok"            >> $DEBBUILDPATH/etc/logrotate.d/$NAME.conf
+echo "	notifempty"           >> $DEBBUILDPATH/etc/logrotate.d/$NAME.conf
+echo "	rotate 4"             >> $DEBBUILDPATH/etc/logrotate.d/$NAME.conf
+echo "	size=10M"             >> $DEBBUILDPATH/etc/logrotate.d/$NAME.conf
+echo "}"                      >> $DEBBUILDPATH/etc/logrotate.d/$NAME.conf
